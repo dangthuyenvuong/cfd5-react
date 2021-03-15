a44f734629d484f19662b7fb63a8efc4349060d0
Dang Thuyen Vuong <dangthuyenvuong@gmail.com>
2021-03-15 23:25:18 +0700

    Update

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,46 @@
+import CourseList from "./components/CourseList";
+import Banner from "./components/Banner";
+import Different from "./components/Different";
+import Terminal from "./components/Terminal";
+import Gallery from "./components/Gallery";
+import Action from "./components/Action";
+import Login from "../../components/Login";
+
+export default function Home() {
+    return (
+        <>
+
+            <main className="homepage" id="main">
+                <Banner />
+                <CourseList />
+                <Different />
+                <Terminal />
+                <Gallery />
+                <Action />
+            </main>
+
+            {/* popup video homepage */}
+            <div className="popup-video" style={{ display: 'none' }}>
+                <div className="wrap">
+                    <div className="video-src" />
+                </div>
+                <div className="close" />
+            </div>
+            <div className="popup-form popup-login" style={{ display: 'none' }}>
+                <div className="wrap">
+                    <h2 className="title">Đăng ký</h2>
+                    <div className="btn btn-icon rect white btn-google">
+                        <img src="/img/google.svg" alt="" />
+                        Google
+                    </div>
+                    <p className="policy">
+                        Bằng việc đăng kí, bạn đã đồng ý <a href="#">Điều khoản bảo mật</a> của CFD
+                    </p>
+                    <div className="close">
+                        <img src="/img/close-icon.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/profile/components/MyCourse.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function MyCourse() {
+    return (
+        <div>
+            <Course />
+        </div>
+    )
+}
+
+
+function Course() {
+    return <div></div>
+}
